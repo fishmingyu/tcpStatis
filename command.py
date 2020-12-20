f3debc0ab5679e5d800c186890602709b401908d
fishming <1661342068@qq.com>
2020-12-20 12:09:32 +0800

    debug TCP

--- command.py
+++ command.py
@@ -1,6 +1,6 @@
 import os
 if os.name == 'posix':
-    os.system("sudo tcpdump -i en0 -G 900 -s 0 -w ./%M_%S.pcap")
+    os.system("sudo tcpdump -i en0 -G 900 -s 0 -w ./data/%M_%S.pcap")
 else:
     os.system("")
 
--- packetReader.py
+++ packetReader.py
@@ -1,28 +1,31 @@
 import pcapy as pc
 import struct
 import util
+import TCPDecode
 import IPv4Decode
 import EthernetDecode
 import csv
 
-reader = pc.open_offline('./data/packet.pcap')
+reader = pc.open_offline('./data/packet2.pcap')
 f = open('./data/data.csv','w',encoding='utf-8',newline='')
 csvWriter = csv.writer(f)
 csvWriter.writerow(util.head())
 
-while(1):
+#while(1):
+for i in range(15):
     (Pkthdr, packet) = reader.next()
     length = len(packet)
     if(length == 0):
         break
-    
     Ethernet = EthernetDecode.Ethernet()
     Ethernet.decodeEthernet(packet[0:14])
     
     if(Ethernet.IPType == '0800'): #exclusively decode IPv4
         IPv4 = IPv4Decode.IPv4()
         IPv4.decodeIP(packet[14:34])
-        csvData = Ethernet.data() + IPv4.data()
-        csvWriter.writerow(csvData)
-
+        if(IPv4.protocol == 6):
+            TCP = TCPDecode.TCP()
+            TCP.decodeTCP(packet[34:])
+            csvData = Ethernet.data() + IPv4.data() + TCP.data()
+            csvWriter.writerow(csvData)
 
